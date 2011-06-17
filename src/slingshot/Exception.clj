4630d7410a674f152c43a939d0451fac294fef1b
Stephen C. Gilardi <scgilardi@gmail.com>
2011-06-17 20:58:07 +0000

    make stack adjustment inside Exception for compatibility

--- src/slingshot/Exception.clj
+++ src/slingshot/Exception.clj
@@ -2,7 +2,11 @@
   (:gen-class :extends RuntimeException
               :state state
               :init init
+              :post-init post-init
               :constructors {[Object] []}))
 
 (defn -init [obj]
   [[] obj])
+
+(defn -post-init [self obj]
+  (->> (.getStackTrace self) (drop 5) into-array (.setStackTrace self)))
