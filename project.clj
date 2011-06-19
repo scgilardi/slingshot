ab1676df5495ba5c72f279c1326037a1b67289a6
Stephen C. Gilardi <scgilardi@gmail.com>
2011-06-19 02:39:40 +0000

    make use of built-in lein-javac official

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject slingshot "0.1.0-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-depencies [[[org.clojars.mmcgrana/lein-javac "1.2.1"]]]
   :java-source-path [["java/src"]])
