0a1312800753bbc4fea5317041542b139367bd25
Stephen C. Gilardi <scgilardi@gmail.com>
2011-10-04 05:03:34 +0000

    irreducibly simple?

--- java/src/slingshot/Stone.java
+++ java/src/slingshot/Stone.java
@@ -3,21 +3,9 @@ package slingshot;
 public class Stone extends RuntimeException {
 
     public final Object context;
-    public final clojure.lang.IFn format;
 
-    public Stone(Object _context, clojure.lang.IFn _format) {
+    public Stone(String message, Throwable cause, Object _context) {
+        super (message, cause);
         context = _context;
-        format = _format;
-    }
-
-    public String getMessage() {
-        Object message;
-        try {
-            message = format.invoke(context);
-        }
-        catch (Exception e) {
-            message = context;
-        }
-        return message.toString();
     }
 }
