b98fb263ae74efd1b400ba857389f249e79db1b7
Stephen C. Gilardi <scgilardi@gmail.com>
2011-10-07 05:29:32 +0000

    bump version past 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slingshot "0.3.0"
+(defproject slingshot "0.3.1-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :java-source-path [["java/src"]])
