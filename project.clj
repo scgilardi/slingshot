ac04e73adc74c170cb8fb6762fc3949ba5892957
Stephen C. Gilardi <scgilardi@gmail.com>
2011-10-20 23:03:16 +0000

    dev after 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slingshot "0.7.0"
+(defproject slingshot "0.7.1-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :java-source-path [["java/src"]])
