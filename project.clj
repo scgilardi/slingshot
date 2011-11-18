37d6c44a92c8166b895edba5260ad4b6f3e913a4
Stephen C. Gilardi <scgilardi@gmail.com>
2011-11-18 23:09:31 +0000

    version 0.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slingshot "0.8.1-SNAPSHOT"
+(defproject slingshot "0.9.0"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :java-source-path [["java/src"]])
