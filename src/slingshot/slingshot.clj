f222f5378f58004872dd76a6d53afa76c27608b4
Stephen C. Gilardi <scgilardi@gmail.com>
2011-12-01 06:07:25 +0000

    move throw context description to get-throw-context

--- src/slingshot/slingshot.clj
+++ src/slingshot/slingshot.clj
@@ -27,45 +27,14 @@
       [<key> <val>] => (= (get % <key>) <val>)
       <predicate>   => (<predicate> %)
 
-  &throw-context is a local acessible within try+ catch clauses. It is
-  a map containing:
   The binding form in a try+ catch clause is not required to be a
   simple symbol. It is subject to destructuring which allows easy
   access to the contents of a thrown collection.
 
-    - for Throwable caught objects:
-      :object       the caught object;
-      :message      the message, from .getMessage;
-      :cause        the cause, from .getCause;
-      :stack-trace  the stack trace, from .getStackTrace;
-      :throwable    the caught object;
-
-    - for non-Throwable caught objects:
-      :object       the caught object;
-      :message      the message, from the optional arguments to throw+;
-      :cause        the cause, captured by throw+, see below;
-      :stack-trace  the stack trace, captured by throw+;
-      :throwable    the outermost Throwable wrapper of the caught object,
-                    see below;
-      :environment  a map from names to values for locals visible at
-                    the throw+ site.
-
-  To throw a non-Throwable object, throw+ wraps it in a Throwable
-  context wrapper. That wrapper may in turn end up wrapped by other
-  exceptions (e.g., instances of RuntimeException or
-  java.util.concurrent.ExecutionException). When processing catch
-  clauses, try+ sees through any such wrappers to find the thrown
-  object. If needed, the outermost wrapper is available within a catch
-  clause via the :throwable key in &throw-context. Any nested wrappers
-  are accessible via its cause chain.
   The hidden local &throw-context is visible within try+ catch
   clauses, bound to the throw context for the caught object.
 
-  When throw+ throws a non-Throwable object from within a try+ catch
-  clause, the outermost wrapper of the caught object being processed
-  is captured as the cause of the new throw+.
-
-  See also throw+"
+  See also: throw+, get-throw-context"
   [& body]
   (let [[expressions catch-clauses finally-clauses] (s/parse-try+ body)]
     `(try
@@ -120,10 +89,43 @@
      `(s/rethrow)))
 
 (defn get-throw-context
-  "Returns a throw context given a Throwable t caught within an
-  ordinary try form. If t or any Throwable in its cause chain is a
-  context wrapper, returns the context with t assoc'd as the value
-  for :throwable, else returns a new context based on t.
+  "Returns the throw context for an object thrown by throw or throw+
+  given a Throwable t. Allows callers to access information about any
+  thrown object as a Clojure map.
+
+  If t or any Throwable in its cause chain wraps a non-Throwable
+  object thrown by throw+, returns its associated context with t
+  assoc'd as the value for :throwable, else returns a new context
+  based on t.
+
+  Within a try+ catch clause, prefer using the &throw-context hidden
+  local to calling get-throw-context explicitly.
+
+  A throw context is a map containing:
+
+    - for Throwable objects:
+      :object       the object;
+      :message      the message, from .getMessage;
+      :cause        the cause, from .getCause;
+      :stack-trace  the stack trace, from .getStackTrace;
+      :throwable    the object;
+
+    - for non-Throwable objects:
+      :object       the object;
+      :message      the message, see throw+;
+      :cause        the cause, see throw+;
+      :stack-trace  the stack trace, see throw+;
+      :environment  the environment, see throw+;
+      :throwable    the outermost Throwable wrapper of the object,
+                    see below;
+
+  To throw a non-Throwable object, throw+ wraps it in a Throwable
+  wrapper. That wrapper may in turn end up wrapped by other
+  exceptions (e.g., instances of RuntimeException or
+  java.util.concurrent.ExecutionException). get-throw-context sees
+  through any such wrappers to find the thrown object. If needed, the
+  outermost wrapper is available via :throwable key in the throw
+  context. Any nested wrappers are accessible via its cause chain.
 
   See also try+"
   [t]
