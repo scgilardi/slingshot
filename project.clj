285e799f4cd67f0f153ad0b6ef7975d6a0ef971f
Stephen C. Gilardi <scgilardi@gmail.com>
2012-01-05 12:07:08 -0500

    :warn-on-reflection in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject slingshot "0.10.1-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :java-source-path [["java/src"]])
+  :java-source-path [["java/src"]]
+  :warn-on-reflection true)
