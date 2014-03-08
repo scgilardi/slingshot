80ea995640262deeb7d6b325de102f85e7cecbd1
Paul Stadig <paul@stadig.name>
2014-03-08 14:38:23 -0500

    Adds support for running tests against multiple Clojure versions.
    Cribbed from clj-http.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject slingshot "0.10.4-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
-  :dev-dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :java-source-path "java/src"
   :java-source-paths ["java/src"]
   :warn-on-reflection true
-  :url "https://github.com/scgilardi/slingshot")
+  :url "https://github.com/scgilardi/slingshot"
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
+  :aliases {"all" ["with-profile" "1.4:1.6"]})
