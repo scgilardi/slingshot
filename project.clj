8581156aa87aeb294e2669a48fd0b70f3c79278c
Paul Stadig <paul@stadig.name>
2014-03-08 17:25:58 -0500

    Make Clojure a dev dependency, and bump to 1.6.0-beta2.

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject slingshot "0.10.4-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[org.clojure/clojure "1.6.0-beta2"]]
   :java-source-path "java/src"
   :java-source-paths ["java/src"]
   :warn-on-reflection true
   :url "https://github.com/scgilardi/slingshot"
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
-  :aliases {"all" ["with-profile" "1.4:1.6"]})
+  :aliases {"all" ["with-profile" "1.4:1.5:1.6"]})
