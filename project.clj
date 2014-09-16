8f5422b7143d0788e4ebe2bd6460713b59ee53d0
Stephen C. Gilardi <scgilardi@gmail.com>
2014-09-16 19:34:05 -0400

    move url to a more prominent spot

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject slingshot "0.10.4-SNAPSHOT"
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
+  :url "https://github.com/scgilardi/slingshot"
   :license {:name "Eclipse Public License 1.0"}
   :dev-dependencies [[org.clojure/clojure "1.6.0-beta2"]]
   :java-source-path "java/src"
   :java-source-paths ["java/src"]
   :warn-on-reflection true
-  :url "https://github.com/scgilardi/slingshot"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
