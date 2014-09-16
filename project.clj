870cdc4f143a79f0ee76e364c4b85d3f98d6cd1a
Stephen C. Gilardi <scgilardi@gmail.com>
2014-09-16 19:37:33 -0400

    update clojure dev dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "Enhanced throw, try, leveraging Clojure's capabilities"
   :url "https://github.com/scgilardi/slingshot"
   :license {:name "Eclipse Public License 1.0"}
-  :dev-dependencies [[org.clojure/clojure "1.6.0-beta2"]]
+  :dev-dependencies [[org.clojure/clojure "1.7.0-alpha2"]]
   :java-source-path "java/src"
   :java-source-paths ["java/src"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}
   :global-vars {*warn-on-reflection* true}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
-  :aliases {"all" ["with-profile" "1.4:1.5:1.6"]})
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}}
+  :aliases {"all" ["with-profile" "1.4:1.5:1.6:1.7"]})
